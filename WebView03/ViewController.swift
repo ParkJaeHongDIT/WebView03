b4173d8b9d95355c3260e4d8d45e700d5658e414
dit02 <dit@imac-7.local>
2019-09-05 14:36:31 +0900

    webView test

--- WebView03/ViewController.swift
+++ WebView03/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let request = URLRequest(url: URL(string: "https://www.naver.com")!)
+        
+        webView.load(request)
     }
-
-
+    @IBAction func backAction(_ sender: Any) {
+        if self.webView.canGoBack{
+        self.webView.goBack()
+        }
+    }
+    
+    
+    @IBAction func forwardAction(_ sender: Any) {
+        if self.webView.canGoForward{
+            self.webView.goForward()
+        }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let inputURL = searchBar.text!
+        let request = URLRequest(url: URL(string: inputURL)!)
+        webView.load(request)
+        self.view.endEditing(true)
+   
+    }
+    
+    
 }
 
